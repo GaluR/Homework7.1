0875e361f3be8e6aac56f3f12701ee9d6bb4c8bd
Bartosz Galinski <galinski.bartosz@gmail.com>
2019-07-25 11:23:42 +0200

    Add class.

--- src/Array.java
+++ src/Array.java
@@ -2,15 +2,17 @@ import java.util.Scanner;
 
 public class Array {
     public static void main(String[] args) {
-        String[] names = new String[5];
+        int a=5;
+        int b=0;
+        String[] names = new String[a];
         Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < a; i++) {
             System.out.println("Wprowadź imię: ");
             names[i] = scanner.nextLine();
         }
         scanner.close();
         System.out.println("Wprowadzone imiona w odwrotnej kolejności to: ");
-        for(int i=4;i>=0;i--){
+        for(int i=a-1;i>=b;i--){
             System.out.println("Cześć " + names[i]);
         }
     }
