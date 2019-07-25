58e6ad1b0c2ec8fad43e69d8379471026f9e8ca4
Bartosz Galinski <galinski.bartosz@gmail.com>
2019-07-25 13:38:19 +0200

    Changes in loops.

--- src/Array.java
+++ src/Array.java
@@ -3,16 +3,15 @@ import java.util.Scanner;
 public class Array {
     public static void main(String[] args) {
         int a=5;
-        int b=0;
         String[] names = new String[a];
         Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i < a; i++) {
+        for (int i = 0; i < names.length; i++) {
             System.out.println("Wprowadź imię: ");
             names[i] = scanner.nextLine();
         }
         scanner.close();
         System.out.println("Wprowadzone imiona w odwrotnej kolejności to: ");
-        for(int i=a-1;i>=b;i--){
+        for(int i=names.length-1;i>=0;i--){
             System.out.println("Cześć " + names[i]);
         }
     }
